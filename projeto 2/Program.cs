6d62b139ca61250df726092fa93f7f2dc2cef756
CJ3016803 <CJ3016803@LAB01-PC42>
2024-02-28 11:10:12 -0300

    Adicionar arquivos de projeto.

---
+++ projeto 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //float salario;
+            //leitura do salario 
+            // Console.WriteLine("Entre com seu salario: ");
+            //salario = float.Parse( Console.ReadLine() );
+
+            // salario = salario + (25.0f / 100.0f) * salario;
+
+            // Console.WriteLine("O novo salario é R$ {0}", salario); ]
+            //Console.ReadKey();
+
+            //atv2
+            //double salario;
+            //Leitura do salario
+            //Console.WriteLine("Digite o valor do salario");
+            //salario = double.Parse(Console.ReadLine());
+            //salario = salario + (25.0 / 100.0) * salario;
+            //Console.WriteLine("o novo salario com aumento de 25 é R$ {0}", salario);
+            //Console.ReadLine();
+
+
+            //atv3
+
+
+        }
+    }
+}
